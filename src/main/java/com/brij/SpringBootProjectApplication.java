dd35f667a22ac495d18c48e872c7bc718d036fbd
Brijesh Patel <patelbrijesh14@gmail.com>
2020-03-08 12:56:22 -0400

    added Log4j

--- src/main/java/com/brij/SpringBootProjectApplication.java
+++ src/main/java/com/brij/SpringBootProjectApplication.java
@@ -1,5 +1,6 @@
 package com.brij;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,14 @@ public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
+		
+		 Logger log=Logger.getLogger(SpringBootProjectApplication.class.getName());
+	       log.debug("Debug");
+	       log.info("Info");
+	       log.warn("Wan");
+	       log.fatal("Fatal");
+	       
+	       System.out.println("Done");
 	}
 
 }
